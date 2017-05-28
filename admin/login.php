03f264db62e5393cd7c11222b9f57a73d2eca572
mohamed hussein <m.hussein1389@gmail.com>
2017-05-29 00:57:09 +0300

    implement login script

--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,11 @@
 <?php require_once('../includes/config.php'); require_once('../includes/loginFunctions.php');
+if(isset($_POST['submit']))
+{
+    $username=mysql_real_escape_string($_POST['username']);
+    $password=mysql_real_escape_string($_POST['password']);
+    
+    login($username,$password);
+}
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -12,6 +19,12 @@
 <div id="wrapper">
 <div id="logo"><a href="./"><img src="../images/logo.png" alt=""/></a></div>
 <div id="content">
+<p> <?php echo printMessages();?></p>
+<form action="" method="post">
+<p><label><strong>UserName: </strong></label> <input type="text" name="username"  size="50"/></p>
+<p><label><strong>Password: </strong></label> <input type="password" name="password"  size="50"/></p>
+<input type="submit" name="submit" value="login" />
+</form>
 </div>
 <div id="footer">Copyrights &copy; 2017 all rights reserved</div>
 </div>
