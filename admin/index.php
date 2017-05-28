35e2d72715a5a8cbd22a23da81002a95aaf19517
mohamed hussein <m.hussein1389@gmail.com>
2017-05-28 23:03:12 +0300

    fix directories error and implement admin panel navigation menu

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<?php require_once('../includes/loginFunctions.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta charset="utf-8"/> 
+	<title>Admin Panel</title>
+</head>
+<link rel="stylesheet" type="text/css" href="../styles/style.css" />
+<body>
+<div id="wrapper">
+<div id="logo"><a href="./"><img src="../images/logo.png" alt=""/></a></div>
+<div id="navigation">
+<ul class="menu">
+<li><a href="../" >view webiste</a></li>
+<li><a href="./logout.php">logout</a></li>
+</ul>
+</div>
+
+<div id="content">
+
+</div>
+<div id="footer">Copyrights &copy; 2017 all rights reserved</div>
+</div>
+</body>
+</html>
