e949f06af9372f2d83571cf2270e79433a4d0418
mohamed hussein <m.hussein1389@gmail.com>
2017-05-28 18:47:49 +0300

    implement navigation content script

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once('includes/config.php');
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -9,7 +11,23 @@
 <body>
 <div id="wrapper">
 <div id="logo"><a href="./"><img src="images/logo.png" alt=""/></a></div>
-<div id="navigation">navigation</div>
+<div id="navigation">
+<ul class="menu">
+<li><a href="./">Home</a></li>
+<?php
+//sql statement to get all other pages 
+$sql="select * from pages where isRoot ='1' order by pageID";
+
+$result=mysql_query($sql) or die("query failed ".mysql_error()); 
+
+while($row=mysql_fetch_object($result))
+{
+    echo '<li><a href="./? p='.$row->pageID.'">'.$row->pageTitle.'</a></li>';
+}
+?>
+</ul>
+</div>
+
 <div id="content">content</div>
 <div id="footer">footer</div>
 </div>
