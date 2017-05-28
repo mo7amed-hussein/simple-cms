d92fd7776d7d7c4a4ceb2a1067a5c576cc913e36
mohamed hussein <m.hussein1389@gmail.com>
2017-05-28 16:39:47 +0300

    adding database credentials

---
+++ admin/config.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author mohamed hussein
+ * @copyright 2017
+ */
+
+
+ob_start();
+
+//start session 
+session_start();
+
+//database credentials
+
+define("DBHost","localhost:3306");
+define("DBUSER","ROOT");
+define("DBPASSWORD","");
+define("DBNAME","mydb");
+
+
+
+
+ 
+?>
