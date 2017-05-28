abdb02729babbdf6fd356960ec214c4e8bd27e8c
mohamed hussein <m.hussein1389@gmail.com>
2017-05-29 00:46:53 +0300

    implement login page main structure

---
+++ admin/login.php
@@ -0,0 +1,19 @@
+<?php require_once('../includes/config.php'); require_once('../includes/loginFunctions.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta charset="utf-8"/> 
+	<title>login</title>
+</head>
+<link rel="stylesheet" type="text/css" href="../styles/style.css" />
+<body>
+<div id="wrapper">
+<div id="logo"><a href="./"><img src="../images/logo.png" alt=""/></a></div>
+<div id="content">
+</div>
+<div id="footer">Copyrights &copy; 2017 all rights reserved</div>
+</div>
+</body>
+</html>
