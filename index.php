b6b32987d1f2e04e537774a7aa8136222b3a2607
mohamed hussein <m.hussein1389@gmail.com>
2017-05-28 18:23:46 +0300

    implement website main stucture

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta charset="utf-8"/> 
+	<title>home</title>
+</head>
+<link rel="stylesheet" type="text/css" href="styles/style.css" />
+<body>
+<div id="wrapper">
+<div id="logo">logo</div>
+<div id="navigation">navigation</div>
+<div id="content">content</div>
+<div id="footer">footer</div>
+</div>
+</body>
+</html>
